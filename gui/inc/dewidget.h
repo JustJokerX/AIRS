19661bbd1ae6b031951673658ff68a4931a211bd
JustJokerX <jiajun.xie.sh@gmail.com>
2017-06-08 17:24:35 +0800

    simple triger added

--- gui/inc/dewidget.h
+++ gui/inc/dewidget.h
@@ -8,6 +8,13 @@ public:
     virtual void InitializePhysics() override;
     virtual void ShutdownPhysics() override;
             void CreateObjects();
+    virtual void CollisionEvent(btRigidBody* pBody0, btRigidBody* pBody1) override;
+protected:
+    // our box to lift
+    GameObject* m_pBox;
+
+    // a simple trigger volume
+    btCollisionObject* m_pTrigger;
 };
 
 #endif // DEWIDGET_H
